2427b85b1223aa0a568f2735b2808875ec099259
asier.pastor <asier.pastor@getronics.com>
2025-07-13 19:17:27 +0200

    Config environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBasePath: 'http://localhost:3000/api', // Cambia esto según tu entorno
+  apiBasePath: 'http://localhost:8080/api', // Cambia esto según tu entorno
 };
